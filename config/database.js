7d91de33a2bff620f2e549673a3a764e2c15e9f8
Sujeet Pandey <sujeetpaandey@gmail.com>
2016-11-24 00:08:38 +0530

    Changed the mongo db config

--- config/database.js
+++ config/database.js
@@ -1,6 +1,4 @@
 module.exports = {
-    //remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/i4vavyHi',
-	//remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/o8nihIso',
-	remoteUrl : 'mongodb://Sujeetpaandey:Chevron3@jello.modulusmongo.net:27017/ojuze9Qu',
+	remoteUrl : 'mongodb://sujeet:pandey@ds050879.mlab.com:50879/finmitra',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
