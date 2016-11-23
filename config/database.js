3338351dce9dd9ff86b0db62b4af8946675f07ae
Sujeet Pandey <sujeetpaandey@gmail.com>
2016-11-23 23:26:38 +0530

    Changes

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
     //remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/i4vavyHi',
-	remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/o8nihIso',
-	//remoteUrl : 'mongodb://node:nodeuser@jello.modulusmongo.net:27017/o8nihIso',
+	//remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/o8nihIso',
+	remoteUrl : 'mongodb://Sujeetpaandey:Chevron3@jello.modulusmongo.net:27017/ojuze9Qu',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
