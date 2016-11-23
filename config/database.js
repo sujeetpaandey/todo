b15ce7c1f648452a8615070b8b63082232a25262
Sujeet Pandey <sujeetpaandey@gmail.com>
2016-11-23 21:11:43 +0530

    First Commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+    //remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/i4vavyHi',
+	remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/o8nihIso',
+	//remoteUrl : 'mongodb://node:nodeuser@jello.modulusmongo.net:27017/o8nihIso',
+    localUrl: 'mongodb://localhost/meanstacktutorials'
+};
